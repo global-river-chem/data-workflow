f5542bc384cb1929a26ba07f85a1735fcfae4a0b
njlyon0 <njlyon@alumni.iastate.edu>
2026-03-01 12:48:47 -0500

    feat (setup): overhaul setup script

--- -setup.r
+++ -setup.r
@@ -5,16 +5,22 @@
 # Centralize setup operations used by many scripts for ease of access
 
 ## ---------------------------------- ##
-# Make Needed Folders ----
+# Make Needed 'Data' Folders ----
 ## ---------------------------------- ##
 
-# Create 'data' folder & sub-folders
-dir.create(path = file.path("data", "preprocess_undone"),
-  showWarnings = F, recursive = T)
-dir.create(path = file.path("data", "preprocess_done"), showWarnings = F)
-dir.create(path = file.path("data", "raw"), showWarnings = F)
-dir.create(path = file.path("data", "standardized"), showWarnings = F)
-dir.create(path = file.path("data", "wrtds"), showWarnings = F)
+# Make relevant top-level folder
+dir.create(file.path("data"), showWarnings = F)
+
+# Make subfolders folders that need chemistry and discharge variants
+purrr::walk(.x = paste0(c("chemistry_", "discharge_"), 
+    sort(rep(c("preprocess-not-done", "preprocess-done", 
+      "raw", "standardized"), times = 2))),
+  .f = ~ dir.create(path = file.path("data", .x),
+    showWarnings = F))
+
+# Make other needed subfolders
+dir.create(file.path("data", "wrtds"), showWarnings = F)
+dir.create(file.path("data", "spatial"), showWarnings = F)
 
 # Clear environment + collect garbage
 rm(list = ls()); gc()
