8f55bc5c58bcfa5311f8a8a3bb89c83a422cf5b4
njlyon0 <njlyon@alumni.iastate.edu>
2026-03-01 14:10:16 -0500

    feat (pre-process): expand script to split 'masterchem' file

--- 00_pre-process/pre-process_glass.r
+++ 00_pre-process/pre-process_glass.r
@@ -21,16 +21,64 @@ rm(list = ls()); gc()
 ## ---------------------------------- ##
 
 # Read in the old 'master' chem data
-chem_v01 <- read.csv(file = file.path("data", "chemistry_preprocess-not-done", "20221030_masterdata_disc_V2.csv"))
+chem_v01 <- read.csv(file = file.path("data", "chemistry_preprocess-not-done", "20221030_masterdata_chem_V2.csv"))
 
 # Check structure
 dplyr::glimpse(chem_v01)
- 
+
+## ---------------------------------- ##
+# Preliminary Wrangling ----
+## ---------------------------------- ##
+
+# Do any generally-useful wrangling
+chem_v02 <- chem_v01 %>% 
+  # Drop unwanted column(s)
+  dplyr::select(-X, -Site.Stream.Name) %>% 
+  # Move variable to a more intuitive spot
+  dplyr::relocate(variable, .before = value) %>% 
+  # Handle special characters in site names
+  dplyr::mutate(
+    # Remove some characters
+    site = gsub(pattern = " |\\(|\\)|<|>", replacement = "", x = site),
+    # Replace others with hyphens
+    site = gsub(pattern = "\\/|\\\\|\\.", replacement = "-", x = site)
+  ) %>% 
+  # Do some renaming
+  dplyr::rename(program = LTER,
+    date = Sampling.Date)
+
+# Check structure
+dplyr::glimpse(chem_v02)
+
 ## ---------------------------------- ##
 # Split By River & Export ----
 ## ---------------------------------- ##
 
+# Iterate across 'LTER'
+for(focal_program in sort(unique(chem_v02$program))){
+  ## focal_program <- "AND"
 
+  # Progress message
+  message("Separating ", focal_program, " rivers")
 
+  # Subset
+  chem_sub.program <- dplyr::filter(chem_v02, program == focal_program)
+
+  # And iterate across rivers within that
+  for(focal_river in sort(unique(chem_sub.program$site))){
+    ## focal_river <- "GSLOOK"
+
+    # Subset again
+    chem_sub.river <- dplyr::filter(chem_sub.program, site == focal_river)
+    
+    # Assemble a good filename
+    focal_out <- paste0(paste("2022-masterchem", focal_program, focal_river, sep = "_"), ".csv")
+
+    # Export locally
+    write.csv(x = chem_sub.river, na = '', row.names = F,
+      file = file.path("data", "chemistry_preprocess-done", focal_out))
+
+  } # Close 'river' loop
+} # Close 'program' loop
 
 # End ----
