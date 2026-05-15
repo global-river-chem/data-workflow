b4895c07b243c09506909fe6b6226b5f56e5cf24
njlyon0 <njlyon@alumni.iastate.edu>
2026-05-15 11:22:55 -0400

    feat (std): add data inventory variables sheet expansion script skeleton

---
+++ 01_standardize/01-C_populate-inventory_variables.r
@@ -0,0 +1,74 @@
+## ----------------------------------------------- ##
+# Populate Data Inventory - Variables Sheet
+## ----------------------------------------------- ##
+## Purpose:
+# Once the river data are in standard formats, we can populate the "variables" sheet of the data inventory
+# This sheet defines:
+## 1. Which variables were measured at each river
+## 2. Measurement units for each variable
+## 3. Minimum Detection Limit (MDL) for each variable
+## 4. First and last years of measurement for each variable
+
+# Get set up
+source(file = file.path("-setup.r"))
+
+# Load libraries
+## install.packages("librarian")
+librarian::shelf(tidyverse)
+
+# Clear environment + collect garbage
+rm(list = ls()); gc()
+
+## ---------------------------------- ##
+# Identify Files Missing in Inventory ----
+## ---------------------------------- ##
+# We only want to do this operation for files not already defined in the 'variables' sheet
+
+# Load relevant piece of data inventory
+invent.var_v01 <- read.csv(file = file.path("data", "data-inventory.csv"))
+
+# Check structure
+dplyr::glimpse(invent.var_v01)
+
+# Identify local standard files
+(local_std <- dir(path = file.path("data", "01_standard")))
+
+# Remove files already described in variables sheet of data inventory
+needed_std <- setdiff(X = (invent.var_v01$standard_filename), y = local_std)
+
+# How many fewer files does that mean we need to process?
+message(length(local_std) - length(needed_std), " fewer files in need of extraction")
+
+## ---------------------------------- ##
+# Extract Variable Info ----
+## ---------------------------------- ##
+
+# Make a list for storing outputs
+var_list <- list()
+
+
+# <`for` loop identifying pre-river, per-variable info here>
+
+
+# Unlist to a dataframe
+var_v01 <- purrr::list_rbind(x = var_list)
+
+# Check structure
+dplyr::glimpse(var_v01)
+
+
+## ---------------------------------- ##
+# Export ----
+## ---------------------------------- ##
+
+# Make one final data object
+var_v99 <- var_v01
+
+# Check structure
+dplyr::glimpse(var_v99)
+
+# Export locally
+write.csv(x = var_v99, na = '', row.names = FALSE,
+  file = file.path("data", paste0(Sys.Date(), "_data-inventory-variable-expansion.csv")))
+
+# End ----
