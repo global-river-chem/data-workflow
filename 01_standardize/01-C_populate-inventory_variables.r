7a5da9c2beec6efb112990686cf6071a79712dc7
njlyon0 <njlyon@alumni.iastate.edu>
2026-05-19 12:20:06 -0400

    feat (std): finalize draft of script for extracting info for data inventory 'variables' sheet

--- 01_standardize/01-C_populate-inventory_variables.r
+++ 01_standardize/01-C_populate-inventory_variables.r
@@ -6,8 +6,8 @@
 # This sheet defines:
 ## 1. Which variables were measured at each river
 ## 2. Measurement units for each variable
-## 3. Minimum Detection Limit (MDL) for each variable
-## 4. First and last years of measurement for each variable
+## 3. First and last years of measurement for each variable
+## 4. Duration of measurement for each variable
 
 # Get set up
 source(file = file.path("-setup.r"))
@@ -25,16 +25,16 @@ rm(list = ls()); gc()
 # We only want to do this operation for files not already defined in the 'variables' sheet
 
 # Load relevant piece of data inventory
-invent_v01 <- readxl::read_excel(path = file.path("data", "data-inventory.xlsx"), sheet = "variables")
+invent.var_v01 <- readxl::read_excel(path = file.path("data", "data-inventory.xlsx"), sheet = "variables")
 
 # Check structure
 dplyr::glimpse(invent.var_v01)
 
 # Identify local standard files
-(local_std <- dir(path = file.path("data", "01_standard")))
+local_std <- dir(path = file.path("data", "01_standard"))
 
 # Remove files already described in variables sheet of data inventory
-needed_std <- setdiff(X = (invent.var_v01$standard_filename), y = local_std)
+(needed_std <- setdiff(x = local_std, y = invent.var_v01$standard_filename))
 
 # How many fewer files does that mean we need to process?
 message(length(local_std) - length(needed_std), " fewer files in need of extraction")
@@ -46,24 +46,36 @@ message(length(local_std) - length(needed_std), " fewer files in need of extract
 # Make a list for storing outputs
 var_list <- list()
 
-# <`for` loop identifying pre-river, per-variable info here>
+# Loop across rivers
+for(focal_river in needed_std){
+  # focal_river <- "australian-govt_australia_barwon_site-01.csv"
 
-# Taken from pre-processing script for master 2026 data, should be perfect with minor adaptations
-# dplyr::group_by(variable) %>% 
-#     dplyr::summarize(
-#       first_year = min(lubridate::year(as.Date(focal_df$date)), na.rm = T),
-#       last_year = max(lubridate::year(as.Date(focal_df$date)), na.rm = T),
-#       .groups = "drop") %>% 
-#     # Also add raw file name
-#     dplyr::mutate(raw_filename = focal_file,
-#       .before = dplyr::everything())
+  # Progress message
+  message("Extracting variable info from ", focal_river)
+
+  # Read in the data file
+  river_data <- read.csv(file = file.path("data", "01_standard", focal_river))
+
+  # Extract needed info in format matching 'variables' sheet of data inventory
+  var_list[[focal_river]] <- river_data %>% 
+    dplyr::group_by(variable, unit) %>% 
+    dplyr::summarize(
+      first_year = min(lubridate::year(as.Date(river_data$date)), na.rm = T),
+      last_year = max(lubridate::year(as.Date(river_data$date)), na.rm = T),
+      .groups = "drop") %>% 
+    dplyr::mutate(standard_filename = focal_river,
+      duration = last_year - first_year,
+      placeholder01 = NA,
+      placeholder02 = NA) %>% 
+    dplyr::select(standard_filename, variable, placeholder01, unit, placeholder02, first_year, last_year, duration)
+
+}
 
 # Unlist to a dataframe
 var_v01 <- purrr::list_rbind(x = var_list)
 
 # Check structure
 dplyr::glimpse(var_v01)
-
 
 ## ---------------------------------- ##
 # Export ----
